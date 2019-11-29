f51e2caf6ce25a88c98a006fc25f5f178df2512f
jean.ferreira <jean.ferreira@aquare.la>
2019-11-29 02:55:57 -0300

    [fix] - correções dos pacotes

--- api/repo/Compras.go
+++ api/repo/Compras.go
@@ -34,13 +34,15 @@ func InsereDados(mov []model.Compra) {
         fmt.Println("Não Conectado!");
     }
 
+    // Se tabela não existe então cria
     if !dbConn.HasTable(&Compra{}) {
+    	fmt.Println("Criando Tabela!");
         dbConn.CreateTable(&Compra{})
     }
 
     for i := 0; i < len(mov); i++ {
 
-    	if (i % 100 == 0){
+    	if (i % 1000 == 0){
     		// var compra Compra
       		fmt.Println("Quantidade de Registros: ", i);
       		// fmt.Println(dbConn.First(&compra, i))
@@ -49,7 +51,7 @@ func InsereDados(mov []model.Compra) {
         ticket := Compra{
                 Cpf_cnpj_comprador: tratamento.RemoveCaracteres(mov[i].Cpf_cnpj_comprador),Flg_private: mov[i].Flg_private, Flg_incompleto: mov[i].Flg_incompleto, Dt_ultima_compra: mov[i].Dt_ultima_compra, Vl_ticket_ult_compra: mov[i].Vl_ticket_ult_compra, Cnpj_loja_freq: tratamento.RemoveCaracteres(mov[i].Cnpj_loja_freq), Cnpj_loja_ultima: tratamento.RemoveCaracteres(mov[i].Cnpj_loja_ultima)}
 
-        dbConn.Save(&ticket)
+        dbConn.Create(&ticket)
     }
 
 }
